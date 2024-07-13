a42e2a9106de00536a91dde77ff9e92bc39cb7d9
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2024-07-13 17:08:02 +0200

    device swap

--- day_4.rs
+++ day_4.rs
@@ -2,7 +2,7 @@
 
 fn main(){
 // test print
-let x = circle_of_life("Coffee".to_string());
+let x = circle_of_life("Big Coffee lalalalalaalalalalalalallaalalalalal".to_string());
 println!("test output step 1 {:#?}",x );
 let (sol_one,sol_two) = do_big_math();
 println!("Solution part one is {}",sol_one);
@@ -24,9 +24,12 @@ fn circle_of_life(input:String)->Vec<u8>{
  let you_mad = input.len();
  let stage_one = denial(input);
  let stage_two = anger(stage_one,you_mad);
+ // lol there was no bargaining imagine having a saying, are you kidding
  let _stage_three = bargaining ();
- let stage_three_too = MD5Buffer::new();
-stage_two
+ let d = depression(stage_two);
+ //stage_four
+let m:Vec<u8> = Vec::new();
+m
 }
 
 
@@ -98,4 +101,71 @@ impl MD5Buffer{
 }
 
 
-//fn depression(){}
+fn depression(process_msg:Vec<u8>)->Vec<u32>{
+	let mut buffer = MD5Buffer::new();
+	// just wtf am I reading in the MD 5 Manual??
+	// whatever happens in step 4 in the guide should be like 4 different steps!
+	// first we create some fun litle bit operator things, easy
+	fn capital_f(x:u32,y:u32,z:u32)->u32{ 
+	(x & y) | (!x & z)
+	}
+
+	fn capital_g(x:u32,y:u32,z:u32)->u32{ 
+	(x & z) | (y & !z)
+	}
+
+	fn capital_h(x:u32,y:u32,z:u32)->u32{ 
+	x ^ y ^ z
+	}
+
+	fn capital_i(x:u32,y:u32,z:u32)->u32{ 
+	y ^ (x | !z)
+	}
+	
+	// turn vec u8 into vec u32 for next steps owo
+	let mut words: Vec<u32> = Vec::new();
+	let mut buffer:u32 = 0;
+	let mut byte_index = 0;
+	
+	for byte in process_msg.iter(){
+	buffer = buffer << 8;
+	buffer |= *byte as u32;
+	byte_index +=1;
+		if byte_index % 4 == 0{
+		words.push(buffer);
+		buffer = 0;	
+		}
+	}
+
+	println!("words{:#?}",words);
+	// get da complicated math
+	let t = acceptance();
+	println!("math{:?}",t);
+
+	for block_index in 0..words.len()/16{ // diveded by 16 only sets the amount of iterations right
+		let block_words = &words[block_index * 16..block_index * 16 + 16]; //  this actually creates the 16 word / bit block, I hope
+
+	aa = buffer.a;
+	bb = buffer.b;
+	cc = buffer.c;
+	dd = buffer.d;
+	}
+	
+
+words
+}
+
+// I was sleeping during math back in school
+// so no idea what im doing
+// why did no one told me math can be beautiful
+fn acceptance()->[u32;64]{
+// docs say this is called t even thos its not a table anymore
+// because we strong
+let mut t = [0u32;64];
+	for i in 0..64{
+	// this number is in docs, don't ask me, me = noob
+	t[i] = (4294967296.0 * ( i as f64 + 1.0).sin().abs()).floor() as u32;
+	}
+	t
+
+}
