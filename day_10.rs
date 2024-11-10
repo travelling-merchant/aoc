c519a61287cd4e9e0725d81d45a824ff88bfcd1a
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2024-11-11 00:11:57 +0100

    fixed the snail, but still not a racing car, but okay

--- day_10.rs
+++ day_10.rs
@@ -1,48 +1,46 @@
-use std::primitive::char;
-fn main(){
-let puzzle_input = String::from("3113322113");
-let result_one = loopholes(puzzle_input);
-println!("Solution 1 is = {}",result_one.len());
+use std::time::Instant;
+fn main() {
+    let start = Instant::now();
+    let puzzle_input = String::from("3113322113");
+    let (result_one, result_two) = loopholes(puzzle_input);
+    println!("Solution 1 is = {}", result_one.len());
+    println!("Solution 2 is = {}", result_two.len());
+    println!("Time taken {:#?}", start.elapsed());
 }
-fn loopholes(mut input:String)->String{
-let mut counter = 0;	
-	for _ in 0..40{
-	counter +=1;
-	println!("counter  is {}",counter);
-	//println!("input is {}",&input);
-	input = get_adjacent(&input);
-	}
-input
-}
-
-
-fn get_adjacent(input:&String)->String{
-let mut same_n_counter:u8 = 1;
-
-let mut strong:String= String::new();
-
-for (i,char) in input.chars().enumerate(){
-	let len_before_end = input.len() -1;
-	if i < len_before_end{
-		if char == input.chars().nth(i+1).expect("could or could not"){
-			same_n_counter +=1;	
-		}
-		else {
-			let amount= format!("{}",same_n_counter);
-			strong.push(amount.chars().nth(0).expect("fml"));
-			strong.push(char);
-			same_n_counter = 1;
-		
-		}
-	}
-	else {
-			let amount= format!("{}",same_n_counter);
-			strong.push(amount.chars().nth(0).expect("fml"));
-			strong.push(char);
-			}
-		
-	
+fn loopholes(mut input: String) -> (String, String) {
+    for _ in 0..40 {
+        input = get_adjacent(&input);
+    }
+    let mut clone_it_yes = input.clone();
+    for _ in 0..10 {
+        clone_it_yes = get_adjacent(&clone_it_yes);
+    }
+    (input, clone_it_yes)
 }
 
-strong
+fn get_adjacent(input: &String) -> String {
+    let mut same_n_counter = 1;
+    let mut strong: String = String::new();
+    let mut basic_counter = 0;
+    let mut m = input.chars().skip(1);
+    for ch in input.chars() {
+        let len_before_end = input.len() - 1;
+        if basic_counter < len_before_end {
+            if ch == m.next().expect("failuew") {
+                same_n_counter += 1;
+            } else {
+                let amount = format!("{}", same_n_counter);
+                strong.push(amount.chars().next().expect("fml"));
+                strong.push(ch);
+                same_n_counter = 1;
+            }
+        } else {
+            let amount = format!("{}", same_n_counter);
+            strong.push(amount.chars().next().expect("fml"));
+            strong.push(ch);
+        }
+        basic_counter += 1;
+    }
+
+    strong
 }
