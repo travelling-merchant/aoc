59fba0558f54564bde2b039f7a4c545ab16a4475
birds-are-corrupted <69503678+birds-are-corrupted@users.noreply.github.com>
2024-11-17 04:24:50 +0100

    actually working validate function, or so I hope

--- day_11.rs
+++ day_11.rs
@@ -8,38 +8,41 @@ fn main() {
 }
 
 fn idk() -> bool {
-    let pw: &str = "hxbxwxba";
+    //let pw: &str = "hxbxwxba";
+    let pw: &str = "ghjaabcc";
     let m = validate(pw);
     m
 }
 fn validate(pw: &str) -> bool {
-    let mut truth_counter = 0;
-    if pw.contains('i') || pw.contains('j') || pw.contains('l') {
+	println!("validating password");
+    if pw.contains('i') || pw.contains('o') || pw.contains('l') {
         return false;
     } else {
-        truth_counter += 1;
+		println!("password check 1 / 3 success");
     }
     let mut p = pw.chars();
 	let p_len = pw.len()-2;
     p.next();
-// overflow
     if !pw
         .chars().take(p_len)
         .any(|c| c == p.next().expect("tired?"))
     {
         return false;
     } else {
-        truth_counter += 1;
+		println!("password check 2 / 3 success");
     }
 let len = ALPHA.len()-2;
 let mut n = 0;
     for _ in 0..len{
-		let three_letter_sequence:String = format!("{:?}",ALPHA.get(n..n+2).expect("but why, BUT ????????"));
-		println!("{}",&three_letter_sequence);
-        if pw.contains(&three_letter_sequence) {
+		let mut exu = String::new();
+		let uwu = ALPHA.get(n..n+3).expect("but why, BUT ????????");
+		for c in uwu{
+		exu.push(*c).to_owned();
+		}
+        if pw.contains(&exu) {
+		println!("password check 3 / 3 success");
             return true;
         }
-		else {return false}
 		n +=1;
 	}
 false
